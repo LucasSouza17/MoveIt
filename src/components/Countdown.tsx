7b63d8b60226c61cd0a1829612bfa198782587ee
Lucas Fernando de Souza <62787018+LucasSouza17@users.noreply.github.com>
2021-02-28 21:54:55 -0300

    Move.it Finish

---
+++ src/components/Countdown.tsx
@@ -0,0 +1,73 @@
+import { useContext } from 'react'
+import { CountdownContext } from '../contexts/CountdownContext'
+import styles from '../styles/components/Countdown.module.css'
+
+export function Countdown() {
+
+  const { 
+    minutes, 
+    seconds, 
+    hasFinished, 
+    isActive, 
+    startCountdown, 
+    resetCountdown 
+  } = useContext(CountdownContext)
+
+  const [minuteLeft, minuteRight] = String(minutes).padStart(2, '0').split('')
+  const [secondLeft, secondRight] = String(seconds).padStart(2, '0').split('')
+
+  return (
+    <div>
+      <div className={styles.countdownContainer}>
+        <div>
+          <span>{minuteLeft}</span>
+          <span>{minuteRight}</span>
+        </div>
+        <span>:</span>
+        <div>
+          <span>{secondLeft}</span>
+          <span>{secondRight}</span>
+        </div>
+      </div>
+
+      { hasFinished ? (
+        <button
+          disabled
+          className={styles.countdownButton}
+        >
+          <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+            Ciclo encerrado
+            <img style={{ marginLeft: '0.7rem' }} src="/icons/check-completed.svg" alt="Completed" />
+          </div>
+        </button>
+      ) : (
+        <>
+          {isActive ? (
+            <button
+              type="button"
+              className={`${styles.countdownButton} ${styles.countdownButtonActive}`}
+              onClick={resetCountdown}
+            >
+              <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+                Abandonar ciclo
+                <svg width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M27 14.41L25.59 13L20 18.59L14.41 13L13 14.41L18.59 20L13 25.59L14.41 27L20 21.41L25.59 27L27 25.59L21.41 20L27 14.41Z" fill="#666666" />
+                </svg>
+              </div>
+            </button>
+          ) : (
+
+            <button
+              type="button"
+              className={styles.countdownButton}
+              onClick={startCountdown}
+            >
+              Iniciar um ciclo
+            </button>
+          )}
+        </>
+      )}
+
+    </div>
+  )
+}
---
+++ src/components/Profile.tsx
@@ -0,0 +1,20 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
+import styles from '../styles/components/Profile.module.css'
+
+export function Profile() {
+  const { level } = useContext(ChallengesContext)
+
+  return (
+    <div className={styles.profileContainer}>
+      <img src="https://github.com/lucassouza17.png" alt="Lucas Souza" />
+      <div>
+        <strong>Lucas Souza</strong>
+        <p>
+          <img src="icons/level.svg" alt="Level" />
+          Level {level}
+        </p>
+      </div>
+    </div>
+  );
+}
